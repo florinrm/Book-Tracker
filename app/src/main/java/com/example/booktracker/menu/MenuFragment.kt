6163409dde910a7a31e859ae4ced95eef4387e08
Florin Mihalache <florinrm@users.noreply.github.com>
2022-01-20 00:03:50 +0200

    Fixed back button for Menu

--- app/src/main/java/com/example/booktracker/menu/MenuFragment.kt
+++ app/src/main/java/com/example/booktracker/menu/MenuFragment.kt
@@ -3,11 +3,12 @@ package com.example.booktracker.menu
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.booktracker.R
@@ -26,6 +27,19 @@ class MenuFragment : Fragment() {
 
         binding.menuModelView = viewModel
         setOnClickListeners()
+
+        binding.root.isFocusableInTouchMode = true
+        binding.root.requestFocus()
+        binding.root.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+                if (event != null && event.action == KeyEvent.ACTION_DOWN) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK) {
+                        return true
+                    }
+                }
+                return false
+            }
+        })
 
         return binding.root
     }
