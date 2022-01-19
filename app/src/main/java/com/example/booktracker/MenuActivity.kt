5d8140be0a99fae55116a0af7ea1069d93710ac0
Florin Mihalache <florinrm@users.noreply.github.com>
2022-01-19 22:38:22 +0200

    Deleted UserAccountActivity.kt and refactored navigation

--- app/src/main/java/com/example/booktracker/MenuActivity.kt
+++ app/src/main/java/com/example/booktracker/MenuActivity.kt
@@ -2,10 +2,12 @@ package com.example.booktracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
         setContentView(R.layout.activity_menu)
     }
 }
